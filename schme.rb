8a49320e7d26ef5eac13401f5e473cb443b8997b
ruslashev <ruslashev@gmail.com>
2013-03-31 17:51:50 +0400

    Now as a REPL

--- schme.rb
+++ schme.rb
@@ -1,36 +1,32 @@
-class Lirb
-  def initialize newFile
-    @file = newFile
-    @exp = []
+Env = {
+  :+      => ->(a, b) { a + b },
+  :quote  => ->(*a)   { a.flatten 1 },
+  :eq?    => ->(a, b) { a == b },
+  :atom?  => ->(a)    { !a.is_a? Array },
+  :list?  => ->(a)    { a.is_a? Array }
+}
 
-    @env = {
-      :+      => ->(a, b) { a + b },
-      :quote  => ->(*a)   { a.flatten 1 },
-      :eq?    => ->(a, b) { a == b },
-      :atom?  => ->(a)    { !a.is_a? Array },
-      :list?  => ->(a)    { a.is_a? Array }
-    }
+def read
+  print "> "
+  exp = []
+  begin
+    Kernel.eval("exp = " + gets.chomp.gsub(/([()])|([^()\d ]+)|(\d+)/) { |s| {"(" => "[", ")" => "]", $2 => ":"+$2.to_s, $3 => $3.to_i.to_s}[s]+(s == "(" ? " " : ",") }.gsub(/,\]/, "]").chop)
+  rescue Exception
+    puts "There are mismatched parentheses."
+    exit
   end
-  
-  def parse
-    begin
-      eval("@exp = " + @file.gsub(/([()])|([^()\d ]+)|(\d+)/) { |s| {"(" => "[", ")" => "]", $2 => ":"+$2.to_s, $3 => $3.to_i.to_s}[s]+(s == "(" ? " " : ",") }.gsub(/,\]/, "]").chop)
-    rescue Exception
-      puts "There are mismatched parentheses."
-      exit
-    end
-    #puts "parsed to \"#{@exp}\""
-  end
-
-  def evaluate exp=@exp
-    head, *rest = exp
-    #puts "got '#{head}' as head and '#{rest.inspect}' as rest"
-
-    rest.map! { |i| i.is_a?(Array) ? evaluate(i) : i }
-    @env[head].call *rest
-  end
+  #puts "parsed to \"#{exp}\""
+  exp
 end
 
-l = Lirb.new "(eq? (+ 1 1) 2)"
-l.parse
-p l.evaluate
+def eval exp
+  head, *rest = exp
+  #puts "got '#{head}' as head and '#{rest.inspect}' as rest"
+
+  rest.map! { |i| i.is_a?(Array) ? eval(i) : i }
+  Env[head].call *rest
+end
+
+loop {
+  print(eval(read), "\n")  # authentic repl
+}
