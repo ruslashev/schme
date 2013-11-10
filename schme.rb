68f8ed54639d011be361e09086fa0fc509e9abcf
ruslashev <ruslashev@gmail.com>
2013-11-10 21:24:06 +0400

    Reformat ruby parser a bit

--- schme.rb
+++ schme.rb
@@ -8,27 +8,24 @@ Env = {
 def read
   print "> "
   exp = []
-  begin
-    Kernel.eval("exp = " + gets.chomp.gsub(/([()])|([^()\d ]+)|(\d+)/) { |s| {"(" => "[", ")" => "]", $2 => ":"+$2.to_s, $3 => $3.to_i.to_s}[s]+(s == "(" ? " " : ",") }.gsub(/,\]/, "]").chop)
-  rescue Interrupt      # Ctrl+C
-    puts "", "Bye!"
-    exit
-  rescue SyntaxError    # (() for example
-    puts "Messed up Parentheses!"
-    exit
-  rescue StandardError  # The rest, also Ctrl+D
-    puts "", "Something gone wrong!"
-    exit
-  end
+  Kernel.eval("exp = " + gets.chomp.gsub(/([()])|([^()\d ]+)|(\d+)/) { |s| {"(" => "[", ")" => "]", $2 => ":"+$2.to_s, $3 => $3.to_i.to_s}[s]+(s == "(" ? " " : ",") }.gsub(/,\]/, "]").chop)
+  p exp
   exp
+rescue Interrupt => e # Ctrl+C
+  exit
+rescue Exception => e
+  print "Syntax Error"
 end
 
 def eval exp
   head, *rest = exp
   rest.map! { |i| i.is_a?(Array) ? eval(i) : i }
-  Env[head].call(*rest) if Env.has_key?(head)
+  if Env.has_key?(head)
+    Env[head].call(*rest)
+  else
+    puts "Unknown identifier \"#{head}\""
+    exit 2
+  end
 end
 
-loop {
-  print(eval(read), "\n")  # authentic repl
-}
+loop { print(eval(read), "\n") } # authentic repl
